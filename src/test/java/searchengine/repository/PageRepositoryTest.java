8664751a4f31702e0da588faf84c88168a7ca04e
Артем <filkof@mail.ru>
2022-11-14 17:18:59 +0300

    Добавил application-test.yml В нем прокинул указатель на changelog

---
+++ src/test/java/searchengine/repository/PageRepositoryTest.java
@@ -0,0 +1,10 @@
+package searchengine.repository;
+
+import org.junit.jupiter.api.Disabled;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("Еще не реализован")
+class PageRepositoryTest {
+
+}
