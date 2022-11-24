d4b0ece28fd2cf6f14f6f1f3171dc6242d841165
Artem Filkov <100158318+Filkoof@users.noreply.github.com>
2022-11-24 18:27:09 +0300

    Ci cd experiment (#3)
    * Подключил H2 к проекту для тестового контекста
    
    * Попытка починить контекст тестов в ci-cd
    
    * Попытка починить контекст тестов в ci-cd

--- src/test/java/searchengine/services/StatisticsServiceTests.java
+++ src/test/java/searchengine/services/StatisticsServiceTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.services.interfaces.StatisticsService;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class StatisticsServiceTests {
 
     @Autowired
